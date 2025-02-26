4acdd404e1fd283822a248b77cfed6855194bd3c
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-02-26 21:14:46 +0000

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    cors: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      clientPort: 3000,
+      host: 'localhost'
+    }
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  }
+}) 
